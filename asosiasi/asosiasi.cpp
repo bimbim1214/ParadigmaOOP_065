2f62d6b784a1e93208c9c7e9ac54f1830cb8a3d2
bimbim1214 <bimoadityapangestu@gmail.com>
2023-05-24 17:07:39 +0700

    di dalam class dokter memiliki enam member

--- asosiasi/asosiasi.cpp
+++ asosiasi/asosiasi.cpp
@@ -17,5 +17,16 @@ class pasien {
 };
 
 class dokter {
+public:
+	string nama;
+	vector<pasien*> daftar_pasien;
 
+	dokter(string pNama) :nama(pNama) {
+		cout << "Dokter \"" << nama << "\" ada\n";
+	}
+	~dokter() {
+		cout << "Dokter \"" << nama << "\" tidak ada\n";
+	}
+	void tambahPasien(pasien*);
+	void cetakPasien();
 };
