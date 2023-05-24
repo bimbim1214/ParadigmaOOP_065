ebe75918f6e8b79f1598a3a54b80deacb6b30aa0
bimbim1214 <bimoadityapangestu@gmail.com>
2023-05-24 18:22:42 +0700

    memanggil program dengan int main

--- mainIA/mainIA.cpp
+++ mainIA/mainIA.cpp
@@ -5,3 +5,24 @@ using namespace std;
 #include "anak.h"
 #include "ibu.h"
 
+int main() {
+	ibu* varIbu = new ibu("dini");
+	ibu* varibu2 = new ibu("novi");
+	anak* varAnak1 = new anak("tono");
+	anak* varAnak2 = new anak("riri");
+	anak* varAnak3 = new anak("Dewi");
+
+	varIbu->cetakAnak();
+	varibu2->cetakAnak();
+
+	varIbu->cetakAnak();
+	varibu2->cetakAnak();
+
+	delete varIbu;
+	delete varibu2;
+	delete varAnak1;
+	delete varAnak2;
+	delete varAnak3;
+
+	return 0;
+}
