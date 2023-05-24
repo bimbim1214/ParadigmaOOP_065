118f5bf0c45efee6ba0ea458ef343b747f3ec289
bimbim1214 <bimoadityapangestu@gmail.com>
2023-05-24 16:46:36 +0700

    dibuat untuk memanggil agar krluar hasil nya

--- Paradigma/Paradigma.cpp
+++ Paradigma/Paradigma.cpp
@@ -36,3 +36,10 @@ public:
     }
 };
 
+int main() {
+    pelajar siswa1("andi laksono", "SMAN 1 BANTUL");
+    cout << siswa1.perkenalan();
+    cout << "hasil =" << siswa1.jumlah(10, 90) << endl;
+
+    return 0;
+}
